23877ae017336bf058ea11c3c82aabbdce514ce5
glory sathwika <114656123+sathwika2201@users.noreply.github.com>
2024-04-27 02:51:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin : "*",
+        origin : ["https://weather-api-seven-omega.vercel.app"],
         methods : ["POST","GET"],
         credentials : true
     }
